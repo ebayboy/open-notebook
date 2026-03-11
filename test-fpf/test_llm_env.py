95a9886582f9d99d4f0c7dc28fdb16701acbadf6
fanpengfei1 <fanpengfei@jd.com>
2026-03-11 13:31:38 +0800

    跑通模型百度文心:

--- test-fpf/test_llm_env.py
+++ test-fpf/test_llm_env.py
@@ -7,7 +7,9 @@ load_dotenv()
 
 # 从环境变量读取配置
 api_key = os.getenv("OPENAI_COMPATIBLE_API_KEY")
-base_url = os.getenv("OPENAI_COMPATIBLE_BASE_URL", "https://api.openai.com/v1")
+base_url = os.getenv("OPENAI_COMPATIBLE_BASE_URL", "")
+
+
 
 print("api_key:", api_key)
 print("base_url:", base_url)
@@ -16,8 +18,9 @@ print("base_url:", base_url)
 client = OpenAI(api_key=api_key, base_url=base_url)
 
 # 调用模型
+# model="Qwen3-30B-A3B-GPTQ-Int4",
 response = client.chat.completions.create(
-    model="Qwen3-30B-A3B-GPTQ-Int4",
+    model="Qwen3.5-35B-A3B-FP8",
     messages=[{"role": "user", "content": "Hello, world!"}],
 )
 
